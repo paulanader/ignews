133ff13434ba46880bb6efd084bf780a35b66950
Paula Nader <paulampaan@gmail.com>
2022-07-02 08:48:21 -0300

    Creating Header

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "../../node_modules/next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Paula Nader</h1>
+    </>
+  );
+}
